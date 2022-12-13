97c484e89eab825d30db77bd45f8f289118c8c7a
Max Klammer <maximilian.klammer@gmail.com>
2022-12-13 16:00:22 +0100

    added some fixes

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ import log from './utils/log.js';
 import path from 'path';
 
 const input = cli.input;
+const flags = cli.flags;
+const { clear, debug } = flags;
 
 const PACKAGES_FOLDER = `packages`;
 const PACKAGE_NAME = `ui`;
@@ -28,38 +30,57 @@ const PACKAGE_PATH = `${PACKAGES_FOLDER}/${PACKAGE_NAME}/src/components`;
 	chalkAnimation.rainbow("Hi Gang, Let's move some components");
 
 	if (input.length === 0) {
-		log.error(`Please provide a component name`);
+		log.error(`No component name provided`);
+		process.exit(1);
 	}
 
 	const [componentName] = input;
 
+	// 0. Checks
 	//check if there is a folder with the name of the input
+	checkFolderExists(componentName);
+	//check if component exports props
+	checkExports(componentName);
+
+	// 1. Get all the paths
+	const paths = getPaths();
+
+	// 2. move folder to targetPath
+	moveComponent(paths.pathToPackage, componentName);
+
+	// 3. update the index file in the ui package
+	updateIndexFile(paths.pathToRepoRoot, componentName);
+
+	// 4. update all the paths in repo
+	updateImports(paths.pathToRepoRoot, componentName, paths.pathToProjectRoot);
+})();
+
+function checkFolderExists(componentName) {
 	if (!fs.readdirSync('./').includes(componentName)) {
 		log.error(`There is no folder with the name ${componentName}`);
 		process.exit(1);
 	}
+}
 
-	const [
-		relativePathToRepositoryRoot,
-		componentTargetPath,
-		pathToProjectRoot
-	] = buildPath();
-
-	//1. move folder to targetPath
-	moveComponent(componentTargetPath, componentName);
-
-	//2. update the index file in the ui package
-	updateIndexFile(relativePathToRepositoryRoot, componentName);
-
-	// 3. update all the paths in repo
-	updateImports(
-		relativePathToRepositoryRoot,
-		componentName,
-		pathToProjectRoot
+function checkExports(componentName) {
+	// Read the file contents
+	const data = fs.readFileSync(
+		`${componentName}/${componentName}.tsx`,
+		'utf8'
 	);
-})();
 
-function buildPath() {
+	// Check if the file contains the string
+	const typeExport = `export type Props`;
+	const interfaceExport = `export interface Props`;
+	if (data.includes(typeExport) || data.includes(interfaceExport)) {
+		log.error(
+			`The component ${componentName} exports Props. Please rename them before moving the component.`
+		);
+		process.exit(1);
+	}
+}
+
+function getPaths() {
 	//check if current folder has a folder called packages
 	//if not, move up one folder and check again and add ../ to path
 	let relativePathToRepositoryRoot = '';
@@ -76,38 +97,43 @@ function buildPath() {
 	//get the path to the project root like /shared/components
 	const pathToProjectRoot = process.env.PWD.match(/(?<=\bprojects\b).*/i)[0];
 
-	return [
-		relativePathToRepositoryRoot,
-		relativePathToRepositoryRoot + PACKAGE_PATH,
-		pathToProjectRoot
-	];
+	return {
+		pathToRepoRoot: relativePathToRepositoryRoot,
+		pathToProjectRoot: pathToProjectRoot,
+		pathToPackage: relativePathToRepositoryRoot + PACKAGE_PATH
+	};
 }
 
-function moveComponent(targetPath, componentName) {
+function moveComponent(pathToPackage, componentName) {
 	//move folder to targetPath
-	fs.rename(`./${componentName}`, `${targetPath}/${componentName}`, err => {
-		if (err) {
-			// Handle any errors
-			log.error(err);
-			process.exit(1);
-		} else {
-			// The folder was moved successfully
-			log.info('Folder moved to new location');
+	fs.rename(
+		`./${componentName}`,
+		`${pathToPackage}/${componentName}`,
+		err => {
+			if (err) {
+				// Handle any errors
+				log.error(err);
+				process.exit(1);
+			} else {
+				// The folder was moved successfully
+				log.info('📦 Folder moved to new location');
+			}
 		}
-	});
+	);
 }
 
-function updateIndexFile(pathToRoot, componentName) {
-	const data = `export * from './src/components/${componentName}';`;
-	const filePath = `${pathToRoot}${PACKAGES_FOLDER}/${PACKAGE_NAME}/index.ts`;
+function updateIndexFile(pathToRepoRoot, componentName) {
+	const data = `export * from './src/components/${componentName}'`;
+	const filePath = `${pathToRepoRoot}${PACKAGES_FOLDER}/${PACKAGE_NAME}/index.ts`;
 
-	fs.appendFile(filePath, data, err => {
-		if (err) {
-			log.error(err);
-		} else {
-			log.info('Added export statement to ui package index file');
-		}
-	});
+	try {
+		fs.appendFileSync(filePath, data);
+	} catch (err) {
+		log.error(err);
+		process.exit(1);
+	}
+
+	log.info('🔝 Added export statement to ui package index file');
 }
 
 function updateImports(root, componentName, pathToProjectRoot) {
@@ -124,10 +150,10 @@ function updateImports(root, componentName, pathToProjectRoot) {
 			}
 		} else {
 			const textToReplace = new RegExp(
-				`@helios${pathToProjectRoot}/${componentName}`,
+				`'@helios${pathToProjectRoot}/${componentName}'`,
 				'g'
 			);
-			const replacement = `${PACKAGE_NAMESPACE}/${PACKAGE_NAME}`;
+			const replacement = `'${PACKAGE_NAMESPACE}/${PACKAGE_NAME}'`;
 			const fileContents = fs.readFileSync(filePath, 'utf8');
 			if (textToReplace.test(fileContents)) {
 				const newFileContents = fileContents.replace(
